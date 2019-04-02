dfa25a1470d21c10d0e563d97b3a71c2a1d2decb
Renee Deka Ambia <dekadekadeka@gmail.com>
2019-04-02 03:07:42 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -2,16 +2,17 @@ def instantiate_new_array
 end
 
 def array_with_two_elements
+  array = Array.new(2)
 end
 
-def first_element
+def first_element(array)
 end
 
-def third_element
+def third_element(array)
 end
 
-def last_element
+def last_element(array)
 end
 
-def first_element_with_array_methods
+def first_element_with_array_methods(array)
 end
